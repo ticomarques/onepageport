7511bbf47525719dc07c4ba2fd86a581021d882a
Tiago Marques <thymarques@gmail.com>
2022-09-22 09:08:51 -1000

    Hero section ready

---
+++ components/Hero.tsx
@@ -0,0 +1,59 @@
+/* eslint-disable @next/next/no-img-element */
+import Link from 'next/link';
+import React from 'react';
+import { Cursor, useTypewriter} from 'react-simple-typewriter';
+import BackgroundCircles from './BackgroundCircles';
+
+type Props = {}
+
+function Hero({}: Props) {
+
+  const [text, count] = useTypewriter({
+    words:[
+        "Hi, my name is Tiago",
+        "Guy-who-loves-Coffee.tsx",
+        "<ButLovesToCodeMore />"
+    ],
+    loop: true,
+    delaySpeed: 2000,
+  });
+
+  return (
+    <div className="h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden">
+      <BackgroundCircles />
+      <img className="relative rounded-full h-32 w-32 mx-auto object-cover" src="https://cdn.sanity.io/images/ltuexkre/production/a766c574c295f2103c63c2dfdf675557c664d016-600x600.png" alt="xxx" />
+      <div className="z-20">
+        <h2 className="text-sm uppercase text-gray-500 pb-2 tracking-[15px]">Software Engineer</h2>
+        <h1>
+          <span className="text-5xl lg:text-6xl font-semibold scroll-px-10">{text}</span>
+          <Cursor cursorColor="#F7AB0A" />
+        </h1>
+        <div className="pt-5">
+          
+          <Link href="#about">
+            <button className="heroButton">About</button>
+          </Link>
+
+          <Link href="#experience">
+            <button className="heroButton">Experience</button>
+          </Link>
+          
+          <Link href="#skills">
+            <button className="heroButton">Skills</button>
+          </Link>
+
+          <Link href="#projects">
+            <button className="heroButton">Projects</button>
+          </Link>
+
+          <Link href="#contact">
+            <button className="heroButton">Contact</button>
+          </Link>
+
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Hero
