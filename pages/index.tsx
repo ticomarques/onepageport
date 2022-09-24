dcd3f79745fd38aee2e4ebc588369adcd58b765a
Tiago Marques <thymarques@gmail.com>
2022-09-24 10:16:53 -1000

    pages ready

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,16 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import About from '../components/About';
+import WorkExperience from '../components/WorkExperience';
 import Header from '../components/Header';
 import Hero from '../components/Hero';
+import Skills from '../components/Skills';
+import Projects from '../components/Projects';
+import ContactMe from '../components/ContactMe';
 
 const Home: NextPage = () => {
   return (
-    <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-scroll z-0">
+    <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-y-scroll overflow-x-hidden z-0">
       <Head>
         <title>Tiago Marques Frontend Engineer website, you can access all information about me, like portfolio, about section and contact. Feel free to contact using the form in the contact section.</title>
         <link rel="icon" href="/favicon.ico" />
@@ -22,13 +26,21 @@ const Home: NextPage = () => {
         <About />
       </section>
 
-      { /** Experience */}
+      <section id="experience" className="snap-center">
+        <WorkExperience />
+      </section>
 
-      { /** Skills */}
+      <section id="skills" className="snap-start">
+        <Skills />
+      </section>
 
-      { /** Projects */}
+      <section id="projects" className="snap-start">
+        <Projects />
+      </section>
 
-      { /** Contact */}
+      <section id="contact" className="snap-start">
+        <ContactMe />
+      </section>
       
     </div>
   )
