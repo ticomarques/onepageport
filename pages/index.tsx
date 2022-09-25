6557be746c5e0467f02306e8e12e58fe88bc51ad
Tiago Marques <thymarques@gmail.com>
2022-09-24 21:55:20 -1000

    frontend ready

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,7 +10,7 @@ import ContactMe from '../components/ContactMe';
 
 const Home: NextPage = () => {
   return (
-    <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-y-scroll overflow-x-hidden z-0">
+    <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-y-scroll overflow-x-hidden z-0 scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80">
       <Head>
         <title>Tiago Marques Frontend Engineer website, you can access all information about me, like portfolio, about section and contact. Feel free to contact using the form in the contact section.</title>
         <link rel="icon" href="/favicon.ico" />
