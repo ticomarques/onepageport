b398e254d5a90d1ea22c42143339a9b36dedab84
Tiago Marques <thymarques@gmail.com>
2022-09-29 20:51:25 -1000

    building local and uploading

---
+++ .next/server/middleware-react-loadable-manifest.js
@@ -0,0 +1,1 @@
+self.__REACT_LOADABLE_MANIFEST={"../node_modules/next-sanity/dist/next-sanity.esm.js -> @sanity/groq-store":{id:4820,files:["static/chunks/68120821.40083050f25ebe9e.js","static/chunks/743.f94f56742feddd19.js"]}};
