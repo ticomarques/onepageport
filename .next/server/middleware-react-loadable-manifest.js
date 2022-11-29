1e77cb7352c51f48b513744639095c8a2b5c79ca
Tiago Marques <thymarques@gmail.com>
2022-11-29 09:17:57 -1000

    removing manual deploy from project

--- .next/server/middleware-react-loadable-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__REACT_LOADABLE_MANIFEST={"../node_modules/next-sanity/dist/next-sanity.esm.js -> @sanity/groq-store":{id:4820,files:["static/chunks/68120821.40083050f25ebe9e.js","static/chunks/743.f94f56742feddd19.js"]}};
--- .next/static/8RDnW6BA21-YwBymi01wz/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/112-5e1e2e0ccf82863d.js","static/chunks/pages/index-19ae4bd4489365ad.js"],"/_error":["static/chunks/pages/_error-e4f561a102d9bb14.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
